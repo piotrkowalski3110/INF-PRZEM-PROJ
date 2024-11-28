c511c16ffdf6abcea5d1d4eb93fbce3e033f5e36
piotrkowalski3110 <piotr.kowalski3110@gmail.com>
2024-11-28 07:49:59 +0100

    compile script

---
+++ compile.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+gcc controller.c -o control -lrt -pthread
+gcc temperature.c -o temp -lrt -pthread
+gcc air_humidity.c -o air -lrt -pthread
+gcc soil_moisture.c -o soil -lrt -pthread
+gcc sunlight.c -o sun -lrt -pthread
